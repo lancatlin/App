70c67d2583f97cd4ea483d28449f5a6b302804c0
lancatlin <lancat1499@gmail.com>
2017-09-07 19:23:05 +0800

    Add files via upload

--- tkmain3.py
+++ tkmain3.py
@@ -6,64 +6,63 @@ def buttoncommend(m):
     titlevar.set('選擇模式: '+buttonvar[m])
     global mode
     mode = m
-
-#建立畫面
-window = tk.Tk()
-window.title('Encoding')
-window.geometry('300x500')
-
-mode=0
-titlevar=tk.StringVar()   #開頭問題變數
-titlelabel=tk.Label(window,textvariable=titlevar).grid(row=1,columnspan=4,sticky=tk.W)   #開頭問題label
-titlevar.set('選擇模式:')
-button=[]
-buttonvar=['加密','解密','文件加密','文件解密']
-for j in range(4):
-    button.append(tk.Button(window,text = buttonvar[j],command=lambda j=j: buttoncommend(j)).grid(row=2,column=j,sticky=tk.N))
-lab1=tk.Label(window,text='請輸入來源與輸出文件名(不需加.txt)').grid(row=3,columnspan=6,sticky=tk.N)
-inputlabel = tk.Label(window,text='來源文件檔:').grid(row=4,columnspan=2,sticky=tk.W)
-inputentry = tk.Entry(window).grid(row=4,column=2,columnspan=4)
-outputlabel = tk.Label(window,text='輸出文件檔:').grid(row=5,columnspan=2,sticky=tk.W)
-outputentry = tk.Entry(window).grid(row=5,column=2,columnspan=4)
-wordlabel=tk.Label(window,text='輸入文字內容:').grid(row=6,columnspan=3,sticky=tk.W)
-wordtext=tk.Text(window,width=35,height=12).grid(row=7,columnspan=6,sticky=tk.N)
-codelabel = tk.Label(window,text='密碼:').grid(row=8,columnspan=1,sticky=tk.W)
-codeentry = tk.Entry(window,width=12).grid(row=8,column=1,columnspan=1)
-
-def start():  #按下按鈕後的程式
-    # 輸入內容
-    print(inputentry)  #print出來的是None
-    text=''
-    if inputentry.get() != '':
+def clean():
+    inputentry.delete(0,'end')
+    outputentry.delete(0,'end')
+    wordtext.delete(1.0,'end')
+    codeentry.delete(0,'end')
+    outputtext.delete(1.0,'end')
+def _print_(t):
+    print(t)
+    printvar.set(str(t))
+def _input_(e):
+    _input = inputentry.get()
+    print(mode)
+    if _input != '':
         try:
-            o=open('file\\'+inputentry.get()+'.txt','r')
-            text+=o.read()+'/n'
+            _input += '.txt'
+            o = open('file\\' + _input, 'r', encoding='utf-8-sig')
+            text = o.read()
+            wordtext.insert(1.0, text)
         except:
-            return 1
-    text+=wordtext.get()
-    print(text)
+            _print_('文件不存在')
+def start():
+    # 輸入內容
+    text=wordtext.get(1.0,'end')
     # 輸入密碼
+    if mode == 0:
+        long=len(text)
+    elif mode == 1:
+        long=''
+        for i in range(len(text)):
+            if text[i] == '!':
+                print (long)
+                text = text.strip(long+'!')
+                break
+            else:
+                long += text[i]
+        long = int(long)
     try:
         password = int(codeentry.get())
     except:
-        return 2
+        _print_('密碼非數字')
     # 輸入輸出檔名
     outdoc = outputentry.get()
     if outdoc != '':
         outdoc += '.txt'
         f = open('file\\' + outdoc, 'w')
         f.close()
-    print()
     # 計算
-    if mode % 2 == 1:
-        text += ' ' * (password - len(text) % password)
+    password %= long
+    while len(text) % password != 0 and mode == 0:
+        text+=' '
     xword = len(text) // password
     result = []
     k = 0
-    if mode == 2 or mode == 4:
+    if mode == 1:
         p = password
         x = xword
-    elif mode == 1 or mode == 3:
+    elif mode == 0:
         p = xword
         x = password
     else:
@@ -73,19 +72,71 @@ def start():  #按下按鈕後的程式
         for j in range(x):
             result[-1] += text[k]
             k += 1
-    string = ''
+    if mode == 0:
+        string = str(long)+'!'
+    else:
+        string = ''
     for i in range(x):
         for j in result:
             string += j[i]
     print(string)
+    outputtext.delete(1.0,'end')
+    outputtext.insert(1.0,string)
     try:
-        string_e = string.encode('utf-8-sig')
         f = open('file\\' + outdoc, 'w', encoding='utf-8-sig')
         f.write(string)
         f.close()
     except:
-        print()
-    x = input('按下Enter結束程序')
+        pass
+window = tk.Tk()
+window.title('Encoding')
+window.geometry('300x400')
 
-enter = tk.Button(window,text='開始!',width=10,command=start).grid(row=8,column=3) #開始按鈕
+mode=None
+titlevar=tk.StringVar()   #開頭問題變數
+titlelabel=tk.Label(window,textvariable=titlevar)
+titlelabel.grid(row=1,columnspan=4,sticky=tk.W)   #開頭問題label
+titlevar.set('選擇模式:')
+button=[]
+buttonvar=['加密','解密']
+for j in range(2):
+    button.append(tk.Button(window,text = buttonvar[j],command=lambda j=j: buttoncommend(j),width=6))
+    button[-1].grid(row=2,column=j,sticky=tk.N)
+lab1=tk.Label(window,text='請輸入來源與輸出文件名(不需加.txt)')
+lab1.grid(row=3,columnspan=6,sticky=tk.W)
+
+inputlabel = tk.Label(window,text='來源文件檔:')
+inputlabel.grid(row=4,columnspan=2,sticky=tk.W)
+
+inputentry = tk.Entry(window)
+inputentry.grid(row=4,column=1,columnspan=4)
+inputentry.bind('<Return>',_input_)
+
+outputlabel = tk.Label(window,text='輸出文件檔:')
+outputlabel.grid(row=5,columnspan=2,sticky=tk.W)
+
+outputentry = tk.Entry(window)
+outputentry.grid(row=5,column=1,columnspan=4)
+
+wordlabel=tk.Label(window,text='輸入文字內容:')
+wordlabel.grid(row=6,columnspan=3,sticky=tk.W)
+
+wordtext=tk.Text(window,width=35,height=6)
+wordtext.grid(row=7,columnspan=6,sticky=tk.W,padx=20)
+
+codelabel = tk.Label(window,text='密碼:')
+codelabel.grid(row=8,columnspan=1,sticky=tk.W)
+
+codeentry = tk.Entry(window,width=12)
+codeentry.grid(row=8,column=1,columnspan=1)
+
+enter = tk.Button(window,text='開始!',width=7,command=start)
+enter.grid(row=8,column=3)
+clear = tk.Button(window,text='清除',width=7,command=clean)
+clear.grid(row=8,column=2)
+printvar=tk.StringVar()
+printlabel=tk.Label(window,textvariable=printvar)
+printlabel.grid(row=9,columnspan=6)
+outputtext = tk.Text(window,width=35,height=6)
+outputtext.grid(row=10,columnspan=6,sticky=tk.W,padx=20)
 window.mainloop()
